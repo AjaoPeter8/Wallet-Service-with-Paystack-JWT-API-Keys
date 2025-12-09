389c70145f9ef8a59ff8b003990c9757f20e4a36
ajaopeter8 <oluwafemipeter02@gmail.com>
2025-12-10 00:54:43 +0100

    updated the port number

--- server.js
+++ server.js
@@ -415,7 +415,7 @@ app.get('/wallet/transactions', authMiddleware, checkPermission('read'), async (
     }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
